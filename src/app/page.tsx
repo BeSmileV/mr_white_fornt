075892e7c6acd5eae5337950573490706344554c
Сергей <b3ssmile@yandex.ru>
2024-12-07 11:07:52 +0300

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,37 +25,39 @@ export default function Page() {
     return (
         <div className={MainStyle.main}>
             <Header onSearch={(data) => setProducts(data)}/>
-            <div className={MainStyle.productList}>
-                {products.map((item, i) => <ProductCard productName={item.name}
-                                                        cost={String(item.price)}
-                                                        imgs={item.images}
-                                                        onClick={() => router.push(`/${item.id}`)}
-                                                        onBuy={async () => {
-                                                            if (isInCart(item.id)) {
-                                                                const response = await deleteCartItemRequest(item.id)
-                                                                if (response) {
-                                                                    setCart(prevState => {
-                                                                        const newState = {...prevState}
-                                                                        newState.items = newState.items.filter(curProduct => curProduct.product.id !== item.id)
-                                                                        return newState
-                                                                    })
+            <div className={MainStyle.productListWrapper}>
+                <div className={MainStyle.productList}>
+                    {products.map((item, i) => <ProductCard productName={item.name}
+                                                            cost={String(item.price)}
+                                                            imgs={item.images}
+                                                            onClick={() => router.push(`/${item.id}`)}
+                                                            onBuy={async () => {
+                                                                if (isInCart(item.id)) {
+                                                                    const response = await deleteCartItemRequest(item.id)
+                                                                    if (response) {
+                                                                        setCart(prevState => {
+                                                                            const newState = {...prevState}
+                                                                            newState.items = newState.items.filter(curProduct => curProduct.product.id !== item.id)
+                                                                            return newState
+                                                                        })
+                                                                    }
+                                                                } else {
+                                                                    const response = await addToCartRequest(item.id)
+                                                                    if (response) {
+                                                                        setCart(prevState => {
+                                                                            const newState = {...prevState}
+                                                                            newState.items = [...newState.items, {
+                                                                                product: item,
+                                                                                quantity: 1
+                                                                            }]
+                                                                            return newState
+                                                                        })
+                                                                    }
                                                                 }
-                                                            } else {
-                                                                const response = await addToCartRequest(item.id)
-                                                                if (response) {
-                                                                    setCart(prevState => {
-                                                                        const newState = {...prevState}
-                                                                        newState.items = [...newState.items, {
-                                                                            product: item,
-                                                                            quantity: 1
-                                                                        }]
-                                                                        return newState
-                                                                    })
-                                                                }
-                                                            }
-                                                        }}
-                                                        isInCart={isInCart(item.id)}
-                                                        key={i}/>)}
+                                                            }}
+                                                            isInCart={isInCart(item.id)}
+                                                            key={i}/>)}
+                </div>
             </div>
         </div>
     )
